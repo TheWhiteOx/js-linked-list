1b51f97c8a3a654be30abd4bf0ec845137cc5d7e
Raymond Luong <raymond.n.luong@gmail.com>
2016-10-11 17:03:54 -1000

    Set up methods

--- linkedList.js
+++ linkedList.js
@@ -4,5 +4,31 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
+	let linkedList = {};
 
+	linkedList.getHead = function() {
+
+	}
+
+	linkedList.getTail = function() {
+
+	}
+
+	linkedList.add = function(value) {
+
+	}
+
+	linkedList.get = function(n) {
+
+	}
+
+	linkedList.remove = function(n) {
+
+	}
+
+	linkedList.insert = function(value, n) {
+		
+	}
+
+	return linkedList;
 }
