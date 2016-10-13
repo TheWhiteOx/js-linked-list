c005cc303fa61ca4159fb95ef5771919b4bc8e0f
Raymond Luong <raymond.n.luong@gmail.com>
2016-10-12 15:05:55 -1000

    mend

--- linkedList.js
+++ linkedList.js
@@ -4,22 +4,24 @@
  * @return {Object} an object exposing methods to be used to manipulate a linked list
  */
 function linkedListGenerator(){
-	let linkedList = {};
+	let linkedList = {
+		head: null,
+		tail: null
+	};
 
 	linkedList.getHead = function() {
-
+		return linkedList.head
 	}
 
 	linkedList.getTail = function() {
-
+		return linkedList.tail
 	}
 
 	linkedList.add = function(value) {
-
+		linkedList.
 	}
 
 	linkedList.get = function(n) {
-
 	}
 
 	linkedList.remove = function(n) {
